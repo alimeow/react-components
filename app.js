7ac2b458a1d9b1c24b5d860a12fab836672e164d
Ali yoho <studycatcat@yahoo.com>
2022-03-08 14:24:28 -0800

    complete. some room for future style change

--- app.js
+++ app.js
@@ -4,11 +4,31 @@ class GroceryListItem extends React.Component {   //what's this React.component?
   //consgructor pass in props
   constructor(props){
     super(props);
+
+    this.state = {
+      done: false
+    };
   }
 
+
+  //function(or property seemingly) of the class
+  onGoroceryItemHoover() {
+    //setState expression... curly brackets inside brackets to use JS?
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+
   render() {
+
+    var liStyle = {
+      //when mouse leave it's still bold? ***
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
     return(
-      <li>{this.props.groceryItem}</li>
+      <li style={liStyle} onMouseOver={this.onGoroceryItemHoover.bind(this)}>{this.props.groceryItem}</li>
     );
   }
 }
@@ -25,5 +45,3 @@ var GroceryList = (props) => (
 //Render this component to the div tag in index.html with an id of app
 // how do I add a title? like <h2>?
 ReactDOM.render(<GroceryList groceryItems={itemList}/>, document.getElementById('app'));
-
-
